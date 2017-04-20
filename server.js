bc4957caaabb1fdb6c5983f19c0a9f84ca19e141
Quinn R Shumaker <quinnshu@uw.edu>
2017-04-20 16:21:49 -0700

    added code to hopefully stop database from DC'ing

--- server.js
+++ server.js
@@ -21,17 +21,40 @@ function getPass() {
 function connectToDb() {
 	
 	console.log("connecting...")
-  var connection = mysql.createConnection({
+  var dbconfig = ({
     user: 'bf229b15bc2a3e',
     password: '478b8184',
     host: 'us-cdbr-iron-east-03.cleardb.net',
     database: 'heroku_155a5011faf681f',
   });
   
+  var connection;
   
-  connection.connect();
-   
-  
+  //copeied from http://stackoverflow.com/questions/20210522/nodejs-mysql-error-connection-lost-the-server-closed-the-connection
+ function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+//end copied code
+
   return connection;
 	/*
 	
