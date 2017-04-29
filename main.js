03723e1fd2e79cce06df152527a3c801a8151f7a
Dimi Dimov <Dimi Dimov>
2017-04-29 15:32:03 -0700

    help

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ $( document ).ready(function() {
 	fetch('10k_most_common.txt').then(function(response) {
   if(response.ok) {
      console.log(response);
-		 console.log(JSON.stringify(response));
+		 console.log(JSON.stringify(response.body));
     }
  else {
     console.log('Network request for products.json failed with response ' + response.status + ': ' + response.statusText);
