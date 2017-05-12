841459d076d35b20c48d891776cc6cd80875ee0f
Quinn R Shumaker <quinnshu@uw.edu>
2017-05-11 17:32:03 -0700

    ugly test

--- main.js
+++ main.js
@@ -172,6 +172,8 @@ $('#login-button').click(function () {
                 //trying not to put the user in double jeopardy, i.e. registering "too" and "tool" as two separate instances
                 //this only works if the shorter of the two instances is inputted first. But I'm dumb someone fix this
                 for (i = 0; i <= containslist.length; i++) {
+                    console.log(containslist[i]);
+                    console.log(commonwords[i]);
                     if (!containslist[i].includes(commonwords[i]) && !commonwords[i].includes(containslist[i])) {
                         containslist.push(commonwords[i]);
                         commonwordcount++;
